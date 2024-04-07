64ba621d63469e2e02466dde4c776ddcba313983
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-04-06 21:33:17 -0300

    feat: POST /reverse implemented

--- src/controllers/home_controller.py
+++ src/controllers/home_controller.py
@@ -38,3 +38,23 @@ def home():
         translate_to=translate_to,
         translated=translated,
     )
+
+
+@home_controller.route("/reverse", methods=["POST"])
+def reverse():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    translated = GoogleTranslator(
+        source=translate_from or "", target=translate_to or ""
+    ).translate(text_to_translate or "")
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=translated,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate,
+    )
