cca326a10088cbdcbddfc4d22c14e52f70ed9167
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-04-06 21:04:15 -0300

    feat: POST / implemented

--- src/controllers/home_controller.py
+++ src/controllers/home_controller.py
@@ -1,11 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
+
 
 home_controller = Blueprint("home_controller", __name__)
 
 
-@home_controller.route("/", methods=["GET"])
+@home_controller.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
+
+        translated = GoogleTranslator(
+            source=translate_from or "", target=translate_to or ""
+        ).translate(text_to_translate or "")
+
+        return render_template(
+            "index.html",
+            languages=LanguageModel.list_dicts(),
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
+        )
     languages = LanguageModel.list_dicts()
     text_to_translate = "O que deseja traduzir?"
     translate_from = "pt"
