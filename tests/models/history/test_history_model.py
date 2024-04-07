187cb0b36411b45653b35581d47d1cfdf65a18bc
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-04-06 23:24:32 -0300

    test: class HistoryModel tests

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,14 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
-def test_request_history():
-    raise NotImplementedError
+def test_request_history(prepare_base):
+    history_list = json.loads(HistoryModel.list_as_json())
+    assert history_list[0]["text_to_translate"] == "Hello, I like videogame"
+    assert history_list[0]["translate_from"] == "en"
+    assert history_list[0]["translate_to"] == "pt"
+    assert history_list[1]["text_to_translate"] == "Do you love music?"
+    assert history_list[1]["translate_from"] == "en"
+    assert history_list[1]["translate_to"] == "pt"
+    assert len(history_list) == 2
